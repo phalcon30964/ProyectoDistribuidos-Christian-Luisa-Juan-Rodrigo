6b15efa35d55ebef718963af9e3a96ba71aa46a5
Christian David Lopez Morcillo <phalcon30964@users.noreply.github.com>
2017-05-26 22:13:27 -0500

    otro

--- aptly_mirror/files/configuracion_mirror.sh
+++ aptly_mirror/files/configuracion_mirror.sh
@@ -2,17 +2,27 @@
 
 #Se recuperan las variables del entorno y se hace un split separando por ",". Se crea un string que configurara el mirror.
 IFS=', ' read -r -a array <<< "${deps}"
-separador=" | "
 configuracion="Priority (required) | Priority (important) | Priority (standard) "
 for paquete in "${array[@]}"
 do
- configuracion=$configuracion$separador$paquete
+ configuracion=$configuracion" | "$paquete
 done
 
 #Se aplica la configuracion al mirror aplty y se actualiza la base de datos.
 aptly mirror edit -filter="$configuracion" mirror-xenial
 aptly mirror update mirror-xenial
 aptly snapshot create mirror-snap-xenial from mirror mirror-xenial
-#Se crea el snapshot y se publica el mirror
-./aptly_expect.sh
+
+#Se crea el snapshot 
+expect -f <(cat <<'EOD'
+spawn aptly publish snapshot mirror-snap-xenial
+expect "Enter passphrase:"
+send -- "password\r"
+expect "Enter passphrase:"
+send -- "password\r"
+expect eof
+EOD
+)
+
+#Se publica el mirror
 aptly serve
